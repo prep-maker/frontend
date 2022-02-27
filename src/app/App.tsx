50ce3dbea1f8db0f88c742c6d27dfc30294ed0bf
hyemin <hmpkk09@gmail.com>
2022-02-28 01:38:53 +0900

    feat: /writing 라우터 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import ErrorAlert from '../common/components/ErrorAlert/ErrorAlert';
+import Writing from '../pages/Writing/Writing';
 
 const App = () => (
   <>
     <ErrorAlert />
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/writing" />
+      <Route path="/writing" element={<Writing />} />
       <Route path="/review" />
     </Routes>
   </>

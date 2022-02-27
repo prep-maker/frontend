06ead2b3852bceaf687d8c2489b39c7c00d58f48
hyemin <hmpkk09@gmail.com>
2022-02-27 16:20:12 +0900

    feat: Input 컴포넌트 구현

---
+++ src/common/components/Input/Input.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+type InputProps = {
+  value: string;
+  placeholder?: string;
+  fontSize?: 'large' | 'regular' | 'small';
+  onChange: (e: React.FormEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({
+  value,
+  placeholder = '',
+  fontSize = 'small',
+  onChange,
+}: InputProps) => {
+  return (
+    <input
+      className={`${styles.input} ${styles[fontSize]}`}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;

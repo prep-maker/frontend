827425a854f2433e2b76b577af1ff2f6d6cc3459
hyemin <hmpkk09@gmail.com>
2022-02-27 22:54:28 +0900

    feat: HttpClient 클래스 작성

---
+++ src/network/http.ts
@@ -0,0 +1,61 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosInstance,
+  AxiosError,
+} from 'axios';
+
+type ClientOption<T> = {
+  body: T;
+  method: 'get' | 'post' | 'put' | 'delete';
+};
+
+export interface IHttpClient {
+  fetch: <T>(url: string, options: ClientOption<T>) => Promise<AxiosResponse>;
+}
+
+class HttpClient implements IHttpClient {
+  private static http: HttpClient;
+  private client: AxiosInstance = axios.create({
+    baseURL: this.baseURL,
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  private constructor(readonly baseURL: string) {}
+
+  static getHttp = (baseURL: string) => {
+    if (!this.http) {
+      this.http = new HttpClient(baseURL);
+    }
+
+    return this.http;
+  };
+
+  fetch = async <T>(
+    url: string,
+    options: ClientOption<T>
+  ): Promise<AxiosResponse> => {
+    const { body, method } = options;
+    const req: AxiosRequestConfig<T> = {
+      url,
+      method,
+      data: body,
+    };
+
+    try {
+      const res: AxiosResponse = await this.client(req);
+      return res.data;
+    } catch (error) {
+      const err = error as AxiosError;
+      if (err.response) {
+        const data = err.response.data;
+        const message = data?.message ? data.message : '네트워크 에러';
+        throw new Error(message);
+      }
+
+      throw new Error('연결 실패');
+    }
+  };
+}
+
+export default HttpClient;

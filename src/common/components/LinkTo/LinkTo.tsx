b0afaacb9c2ea42448befaf98dda6a9d08e073bb
hyemin <hmpkk09@gmail.com>
2022-02-28 02:34:40 +0900

    feat: memo LinkTo

--- src/common/components/LinkTo/LinkTo.tsx
+++ src/common/components/LinkTo/LinkTo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './LinkTo.module.css';
 
@@ -15,4 +15,4 @@ const LinkTo = ({ value, url }: LinkProps) => {
   );
 };
 
-export default LinkTo;
+export default memo(LinkTo);

67ab3879688a2c1009395838937045edd72570d4
hyemin <hmpkk09@gmail.com>
2022-02-28 02:07:37 +0900

    feat: useAuthService 훅에 onLogin 추가

--- src/features/user/hooks/useAuthService.ts
+++ src/features/user/hooks/useAuthService.ts
@@ -1,25 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { User } from '../type';
+import { LoginInfo, User, UserData } from '../type';
 import AuthAPI from '../api';
 import { fetchUser } from '../userSlice';
 import { alertError } from '../../ui/uiSlice';
 import { useAppDispatch } from '../../../common/hooks/useRedux';
 import useApi from '../../../common/hooks/useApi';
 
-type Handler = {
+type AuthHandler = {
   onSignup: (user: User) => Promise<void>;
+  onLogin: (user: Omit<User, 'name'>) => Promise<void>;
 };
 
 const authApi = useApi(AuthAPI);
 
-const useAuthService = (): Handler => {
+const useAuthService = (): AuthHandler => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const onSignup = async (user: User): Promise<void> => {
     try {
-      const userData = await authApi.signup(user);
+      const userData: UserData = await authApi.signup(user);
       dispatch(fetchUser(userData));
       navigate('/writing');
     } catch (error) {
@@ -28,7 +29,18 @@ const useAuthService = (): Handler => {
     }
   };
 
-  return { onSignup };
+  const onLogin = async (user: LoginInfo): Promise<void> => {
+    try {
+      const userData: UserData = await authApi.login(user);
+      dispatch(fetchUser(userData));
+      navigate('/writing');
+    } catch (error) {
+      const err = error as Error;
+      dispatch(alertError(err.message));
+    }
+  };
+
+  return { onSignup, onLogin };
 };
 
 export default useAuthService;

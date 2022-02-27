47f43d45ef33cf1e663a029823c93384eab81e2d
hyemin <hmpkk09@gmail.com>
2022-02-27 22:52:20 +0900

    feat: Input 컴포넌트 onChange prop 타입 업데이트

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import styles from './Input.module.css';
 
 export type InputProps = {
+  type: 'email' | 'text' | 'password';
+  name: string;
   value: string;
   placeholder?: string;
+  minLength?: number;
+  maxLength?: number;
+  required?: boolean;
   fontSize?: 'large' | 'regular' | 'small';
-  onChange: (e: React.FormEvent<HTMLInputElement>) => void;
+  onChange: React.FormEventHandler;
 };
 
 const Input = ({
+  type,
+  name,
   value,
   placeholder = '',
+  minLength = 0,
+  maxLength = Number.MAX_SAFE_INTEGER,
+  required = false,
   fontSize = 'small',
   onChange,
 }: InputProps) => {
   return (
     <input
       className={`${styles.input} ${styles[fontSize]}`}
+      type={type}
+      name={name}
       value={value}
       placeholder={placeholder}
+      minLength={minLength}
+      maxLength={maxLength}
+      required={required}
       onChange={onChange}
     />
   );

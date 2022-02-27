e5892f9adad5596c3c6b5fd20b8c920759aaa1c4
hyemin <hmpkk09@gmail.com>
2022-02-27 22:51:45 +0900

    feat: ErrorAlert 컴포넌트 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import ErrorAlert from '../common/components/ErrorAlert/ErrorAlert';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/writing" />
-    <Route path="/review" />
-  </Routes>
+  <>
+    <ErrorAlert />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/writing" />
+      <Route path="/review" />
+    </Routes>
+  </>
 );
 
 export default App;
---
+++ src/common/components/ErrorAlert/ErrorAlert.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import styles from './ErrorAlert.module.css';
+import { useAppSelector, useAppDispatch } from '../../hooks/useRedux';
+import { alertError } from '../../../features/ui/uiSlice';
+
+const ErrorAlert = () => {
+  const error = useAppSelector((state) => state.ui.error);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (error) {
+      timer = setTimeout(() => {
+        dispatch(alertError(''));
+      }, 3000);
+    }
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div>{error && <div className={styles.banner}>{error}</div>}</div>;
+};
+
+export default ErrorAlert;

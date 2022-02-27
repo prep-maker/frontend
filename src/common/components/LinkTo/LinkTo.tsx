55ab62a5dfe1e90e9c985cb5915ea3b495168d60
hyemin <hmpkk09@gmail.com>
2022-02-27 23:33:38 +0900

    feat: LinkTo 컴포넌트 구현

---
+++ src/common/components/LinkTo/LinkTo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './LinkTo.module.css';
+
+type LinkProps = {
+  value: string;
+  url: '/login' | '/writing' | '/signup' | '/review';
+};
+
+const LinkTo = ({ value, url }: LinkProps) => {
+  return (
+    <Link to={url} className={styles.link}>
+      {value}
+    </Link>
+  );
+};
+
+export default LinkTo;

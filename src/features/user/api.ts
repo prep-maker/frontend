6b9877190ac966a048217fd8d9f1b0295341d88a
hyemin <hmpkk09@gmail.com>
2022-02-28 02:28:04 +0900

    refactor: signup API 매개변수 이름 변경

--- src/features/user/api.ts
+++ src/features/user/api.ts
@@ -5,12 +5,12 @@ import { IAuth, User, UserData } from './type';
 class AuthAPI implements IAuth {
   constructor(private http: IHttpClient) {}
 
-  signup = async (req: User): Promise<UserData> => {
+  signup = async (user: User): Promise<UserData> => {
     const result: AxiosResponse<UserData> = await this.http.fetch(
       '/auth/signup',
       {
         method: 'post',
-        body: req,
+        body: user,
       }
     );
     return result.data;

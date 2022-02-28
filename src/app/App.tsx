c5f777b773561c9728ebb3837643a10865ba7cb7
hyemin <hmpkk09@gmail.com>
2022-02-28 14:07:54 +0900

    feat: '/' 리다이렉트 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import ErrorAlert from '../common/components/ErrorAlert/ErrorAlert';
 import Writing from '../pages/Writing/Writing';
 import LoginForm from '../features/user/components/LoginForm/LoginForm';
 import SignupForm from '../features/user/components/SignupForm/SignupForm';
 
-const App = () => (
-  <>
-    <ErrorAlert />
-    <Routes>
-      <Route element={<Login />}>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
-      </Route>
-      <Route path="/writing" element={<Writing />} />
-      <Route path="/review" />
-    </Routes>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <ErrorAlert />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route element={<Login />}>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Route>
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/review" />
+      </Routes>
+    </>
+  );
+};
 
 export default App;

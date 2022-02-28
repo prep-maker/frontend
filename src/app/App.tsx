8fa857c5d287094cdae10f542a26500304b3954f
hyemin <hmpkk09@gmail.com>
2022-02-28 11:08:36 +0900

    feat: signUp, login 라우트 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
+import Login from '../pages/Login/Login';
 import ErrorAlert from '../common/components/ErrorAlert/ErrorAlert';
 import Writing from '../pages/Writing/Writing';
+import LoginForm from '../features/user/components/LoginForm/LoginForm';
+import SignupForm from '../features/user/components/SignupForm/SignupForm';
 
 const App = () => (
   <>
     <ErrorAlert />
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route element={<Login />}>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Route>
       <Route path="/writing" element={<Writing />} />
       <Route path="/review" />
     </Routes>

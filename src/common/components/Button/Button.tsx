f29b74578b4448c4c9e5811aea8f83f64fc18487
hyemin <hmpkk09@gmail.com>
2022-03-02 16:47:10 +0900

    feat: Button 컴포넌트 색상 및 circle prop 추가

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,24 +1,29 @@
-import React, { memo } from 'react';
+import React, { memo, ReactElement } from 'react';
+import classNames from 'classnames/bind';
 import styles from './Button.module.css';
 
 type ButtonProps = {
-  value: string;
-  color: 'pink' | 'yellow' | 'green' | 'blue';
+  value: string | ReactElement;
+  color: 'pink' | 'yellow' | 'green' | 'blue' | 'transparent';
   size: 'atom' | 'short' | 'middle' | 'long';
+  circle?: boolean;
   onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
 };
+
+const cn = classNames.bind(styles);
 
 const Button = ({
   value,
   color,
   size,
+  circle = false,
   onClick,
   disabled = false,
 }: ButtonProps) => {
   return (
     <button
-      className={`${styles.button} ${styles[color]} ${styles[size]}`}
+      className={cn('button', color, size, { circle: circle })}
       onClick={onClick}
       disabled={disabled}
     >

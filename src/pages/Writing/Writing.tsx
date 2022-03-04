d5aac13d837ab543b7170a131a68d4a9e0a83888
hyemin <hmpkk09@gmail.com>
2022-03-04 20:31:46 +0900

    feat: Writing 페이지 레이아웃 구현

--- src/pages/Writing/Writing.tsx
+++ src/pages/Writing/Writing.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import WritingList from '../../features/writings/components/WritingList/WritingList';
+import Header from '../../common/components/Header/Header';
+import { useAppDispatch, useAppSelector } from '../../common/hooks/useRedux';
+import useWritings from '../../features/writings/hooks/useWritings';
+import { fetchEditingWritingsByUserId } from '../../features/writings/writingsSlice';
+import styles from './Writing.module.css';
+import WritingSkeleton from './WritingSkeleton';
+import WritingTitle from '../../features/writings/components/WritingTitle/WritingTitle';
+import Button from '../../common/components/Button/Button';
 
 const Writing = () => {
-  return <div>Writing</div>;
+  const dispatch = useAppDispatch();
+  const userId = useAppSelector(({ user }) => user.id);
+  const isLoading = useAppSelector(({ ui }) => ui.loading) === 'pending';
+  const writings = useWritings();
+
+  useEffect(() => {
+    dispatch(fetchEditingWritingsByUserId(userId));
+  }, [userId]);
+
+  return (
+    <>
+      <Header />
+      {isLoading ? (
+        <WritingSkeleton />
+      ) : (
+        <main className={styles.main}>
+          <WritingList writings={writings} />
+          <section className={styles.writing}>
+            <WritingTitle />
+            <div className={styles.editor} />
+            <div className={styles.buttons}>
+              <Button
+                value="삭제"
+                color="magenta"
+                size="short"
+                onClick={() => {}}
+              />
+              <div className={styles.row}>
+                <Button
+                  value="완료"
+                  color="yellow"
+                  size="short"
+                  onClick={() => {}}
+                />
+                <Button
+                  value="저장"
+                  color="blue"
+                  size="short"
+                  onClick={() => {}}
+                />
+              </div>
+            </div>
+          </section>
+        </main>
+      )}
+    </>
+  );
 };
 
 export default Writing;

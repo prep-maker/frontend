829a2a4901bbb8e61d0c096ee223f1232ad16d81
hyemin <hmpkk09@gmail.com>
2022-03-04 21:26:11 +0900

    feat: writingAPI에 update 메소드 추가

--- src/features/writings/writingAPI.ts
+++ src/features/writings/writingAPI.ts
@@ -1,5 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { IHttpClient } from '../../network/http';
+import { Writing } from './writingsSlice';
 
 type Block = {
   id: string;
@@ -21,6 +22,10 @@ export type WritingResponse = {
 export interface IWritingAPI {
   readonly getEditingByUserId: (userId: string) => Promise<WritingResponse[]>;
   readonly getDoneByUserId: (userId: string) => Promise<WritingResponse[]>;
+  readonly update: (
+    userId: string,
+    writing: Omit<Writing, 'blocks'>
+  ) => Promise<WritingResponse>;
 }
 
 class WritingAPI implements IWritingAPI {
@@ -43,6 +48,19 @@ class WritingAPI implements IWritingAPI {
       {
         method: 'get',
       }
+    );
+
+    return result.data;
+  };
+
+  update = async (
+    userId: string,
+    writing: Omit<Writing, 'blocks'>
+  ): Promise<WritingResponse> => {
+    console.log(writing);
+    const result: AxiosResponse<WritingResponse> = await this.http.fetch(
+      `/users/${userId}/writings/${writing.id}`,
+      { method: 'put', body: writing }
     );
 
     return result.data;

45166fe0df0c578a94628f2aa83fd00a383071c8
hyemin <hmpkk09@gmail.com>
2022-03-04 15:14:52 +0900

    feat: store에 thunk config 설정

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,6 +4,9 @@ import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 import uiReducer from '../features/ui/uiSlice';
 import userReducer from '../features/user/userSlice';
+import AuthAPI from '../features/user/authAPI';
+import HttpClient from '../network/http';
+import config from '../common/utils/config';
 
 const reducers = combineReducers({
   ui: uiReducer,
@@ -17,9 +20,19 @@ const persisitConfig = {
 
 const persistedReducer = persistReducer(persisitConfig, reducers);
 
+const http = HttpClient.getHttp(config.baseUrl);
+const authAPI = new AuthAPI(http);
+
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: {
+          authAPI,
+        },
+      },
+    }).concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

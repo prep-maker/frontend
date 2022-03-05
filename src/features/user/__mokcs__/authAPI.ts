29e712660f3c2816cad5d339164987c6e0815d42
hyemin <hmpkk09@gmail.com>
2022-03-05 23:12:02 +0900

    test: AuthAPI 클래스 모킹

---
+++ src/features/user/__mokcs__/authAPI.ts
@@ -0,0 +1,26 @@
+import { IAuthAPI } from '../authAPI';
+import { LoginInfo, User, UserState } from '../types';
+
+const USER_ID = '622337793d451494bbb1c0c9';
+const TOKEN = 'token';
+const USER_NAME = 'username';
+
+class AuthAPIStub implements IAuthAPI {
+  constructor() {}
+
+  signup = async (user: User): Promise<UserState> => ({
+    id: USER_ID,
+    email: user.email,
+    name: user.name,
+    token: TOKEN,
+  });
+
+  login = async (user: LoginInfo): Promise<UserState> => ({
+    id: USER_ID,
+    email: user.email,
+    name: USER_NAME,
+    token: TOKEN,
+  });
+}
+
+export default AuthAPIStub;

3150c72b94dfe80b4621736a04d7076306cc73b4
hyemin <hmpkk09@gmail.com>
2022-03-05 23:43:29 +0900

    test: SignupForm 컴포넌트 테스트

--- src/features/user/tests/SignupForm.test.tsx
+++ src/features/user/tests/SignupForm.test.tsx
@@ -2,45 +2,44 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
 
 import { store } from '../../../app/store';
 import { PLACEHOLDER } from '../../../common/constants/auth';
-import LoginForm from '../components/LoginForm/LoginForm';
+import SignupForm from '../components/SignupForm/SignupForm';
 
 describe('LoginForm Component', () => {
   beforeEach(() => {
     render(
-      <MemoryRouter>
-        <Provider store={store}>
-          <LoginForm />
-        </Provider>
-      </MemoryRouter>
+      <Provider store={store}>
+        <SignupForm />
+      </Provider>
     );
   });
 
-  it('이메일과 비밀번호를 입력받는다.', () => {
+  it('이메일과 이름과 비밀번호를 입력받는다.', () => {
     const emailInput = screen.getByPlaceholderText(
       PLACEHOLDER.EMAIL
     ) as HTMLInputElement;
     const passwordInput = screen.getByPlaceholderText(
       PLACEHOLDER.PASSWORD
     ) as HTMLInputElement;
+    const nameInput = screen.getByPlaceholderText(
+      PLACEHOLDER.NAME
+    ) as HTMLInputElement;
 
     userEvent.click(emailInput);
     userEvent.keyboard('hyemin@email.com');
     userEvent.click(passwordInput);
     userEvent.keyboard('password123');
+    userEvent.click(nameInput);
+    userEvent.keyboard('혜민');
 
     expect(emailInput.value).toBe('hyemin@email.com');
     expect(passwordInput.value).toBe('password123');
+    expect(nameInput.value).toBe('혜민');
   });
 
-  it('이메일과 비밀번호가 입력되지 않으면 로그인 버튼이 활성화되지 않는다.', () => {
-    expect(screen.getByText('로그인')).toBeDisabled();
-  });
-
-  it('회원가입 링크가 있다.', () => {
-    expect(screen.getByText('회원가입')).toBeInTheDocument();
+  it('이메일과 비밀번호와 이름을 입력받지 않으면 가입하기 버튼이 활성화되지 않는다.', () => {
+    expect(screen.getByText('가입하기')).toBeDisabled();
   });
 });

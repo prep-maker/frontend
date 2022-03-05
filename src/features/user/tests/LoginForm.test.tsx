d6c9be539308e0c765783c065bee11bb0f34909c
hyemin <hmpkk09@gmail.com>
2022-03-05 23:12:22 +0900

    test: LoginForm 컴포넌트 테스트

---
+++ src/features/user/tests/LoginForm.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+import { store } from '../../../app/store';
+import LoginForm from '../components/LoginForm/LoginForm';
+
+describe('LoginForm Component', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      </MemoryRouter>
+    );
+  });
+
+  it('이메일과 비밀번호를 입력받는다.', () => {
+    const emailInput = screen.getByPlaceholderText(
+      /이메일/g
+    ) as HTMLInputElement;
+    const passwordInput = screen.getByPlaceholderText(
+      /비밀번호/g
+    ) as HTMLInputElement;
+
+    userEvent.click(emailInput);
+    userEvent.keyboard('hyemin@email.com');
+    userEvent.click(passwordInput);
+    userEvent.keyboard('password123');
+
+    expect(emailInput.value).toBe('hyemin@email.com');
+    expect(passwordInput.value).toBe('password123');
+  });
+
+  it('이메일과 비밀번호가 입력되지 않으면 로그인 버튼이 활성화되지 않는다.', () => {
+    expect(screen.getByText('로그인')).toBeDisabled();
+  });
+
+  it('회원가입 링크가 있다.', () => {
+    expect(screen.getByText('회원가입')).toBeInTheDocument();
+  });
+});

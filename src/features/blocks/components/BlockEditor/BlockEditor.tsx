edc74ce3eea4893974d53f362646ccbf9b1f19eb
hyemin <hmpkk09@gmail.com>
2022-03-06 22:00:32 +0900

    feat: BlockEditor 컴포넌트 구현

---
+++ src/features/blocks/components/BlockEditor/BlockEditor.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from 'react';
+import BlockEditorHeader from '../BlcokEditorHeader/BlockEditorHeader';
+import BlockList from '../BlockList/BlockList';
+import styles from './BlockEditor.module.css';
+
+const BlockEditor = () => {
+  return (
+    <section className={styles.wrapper}>
+      <BlockEditorHeader />
+      <BlockList />
+    </section>
+  );
+};
+
+export default memo(BlockEditor);

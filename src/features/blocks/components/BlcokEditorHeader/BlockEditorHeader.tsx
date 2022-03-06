c9af12ffd5550a0dbd1ef4ec5e66a97b37ecd8b8
hyemin <hmpkk09@gmail.com>
2022-03-06 13:57:50 +0900

    feat: BlockEditorHeader 컴포넌트 구현

--- src/features/blocks/components/BlcokEditorHeader/BlockEditorHeader.tsx
+++ src/features/blocks/components/BlcokEditorHeader/BlockEditorHeader.tsx
@@ -1,15 +1,52 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 
 import Button from '../../../../common/components/Button/Button';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../../../common/hooks/useRedux';
+import useCurrentWriting from '../../../writings/hooks/useCurrentWriting';
+import { ParagraphType } from '../../types';
+import { createBlock } from '../../actions';
 import styles from './BlockEditorHeader.module.css';
 
 const BlockEditorHeader = () => {
+  const dispatch = useAppDispatch();
+  const userId = useAppSelector(({ user }) => user.id);
+  const writingId = useCurrentWriting()?.id;
+
+  const handleClick = useCallback(
+    (types: ParagraphType[]) =>
+      dispatch(createBlock({ userId, writingId, types })),
+    [userId, writingId]
+  );
+
   return (
     <header className={styles.wrapper}>
-      <Button color="blue" value="PREP" size="middle" onClick={() => {}} />
-      <Button color="pink" value="P" size="atom" onClick={() => {}} />
-      <Button color="green" value="R" size="atom" onClick={() => {}} />
-      <Button color="yellow" value="E" size="atom" onClick={() => {}} />
+      <Button
+        color="blue"
+        value="PREP"
+        size="middle"
+        onClick={() => handleClick(['P', 'R', 'E', 'P'])}
+      />
+      <Button
+        color="pink"
+        value="P"
+        size="atom"
+        onClick={() => handleClick(['P'])}
+      />
+      <Button
+        color="green"
+        value="R"
+        size="atom"
+        onClick={() => handleClick(['R'])}
+      />
+      <Button
+        color="yellow"
+        value="E"
+        size="atom"
+        onClick={() => handleClick(['E'])}
+      />
     </header>
   );
 };

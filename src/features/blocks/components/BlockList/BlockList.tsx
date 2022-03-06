fc0a2220618538f065c191447e0de1a16273fa3d
hyemin <hmpkk09@gmail.com>
2022-03-06 23:30:49 +0900

    feat: block이 없을 때 보이는 안내 메세지 업데이트

--- src/features/blocks/components/BlockList/BlockList.tsx
+++ src/features/blocks/components/BlockList/BlockList.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 
 import useCurrentBlocks from '../../hooks/useCurrentBlocks';
 import BlockItem from '../BlockItem/BlockItem';
+import styles from './BlockList.module.css';
 
 const BlockList = () => {
   const blocks = useCurrentBlocks();
@@ -17,7 +18,7 @@ const BlockList = () => {
           ))}
         </ul>
       ) : (
-        <div>블록이 없습니다.</div>
+        <div className={styles.message}>내용이 없습니다.</div>
       )}
     </>
   );

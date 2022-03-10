9a0b875d7dc9605d7b3cb28ae0b3ff12f4c7f2b0
hyemin <hmpkk09@gmail.com>
2022-03-10 23:19:29 +0900

    feat: ParagraphEditor 컴포넌트 구현

---
+++ src/features/reviews/components/ParagraphEditor/ParagraphEditor.tsx
@@ -0,0 +1,49 @@
+import React, { FormEvent } from 'react';
+import { useParams } from 'react-router-dom';
+
+import SwitchableInput from '../../../../common/components/SwitchableInput/SwitchableInput';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../../../common/hooks/useRedux';
+import { saveBlocks } from '../../../blocks/actions';
+import { updateParagraph } from '../../../blocks/blocksSlice';
+import useBlocksByWritingId from '../../../blocks/hooks/useBlocksByWritingId';
+import styles from './ParagraphEditor.module.css';
+
+type ParagraphEditorProp = {
+  blockId: string;
+  index: number;
+};
+
+const ParagraphEditor = ({ blockId, index }: ParagraphEditorProp) => {
+  const dispatch = useAppDispatch();
+  const content = useAppSelector(
+    ({ blocks }) => blocks.byId[blockId].paragraphs[index].content
+  );
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    dispatch(updateParagraph({ blockId, index, value: e.currentTarget.value }));
+  };
+
+  const { writingId } = useParams();
+  const blocks = useBlocksByWritingId(writingId as string);
+
+  const handleEnter = () => {
+    dispatch(updateParagraph({ blockId, index, value: content }));
+    dispatch(saveBlocks({ writingId: writingId as string, blocks }));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.index}>{index + 1}</div>
+      <SwitchableInput
+        value={content}
+        onChange={handleChange}
+        onEnter={handleEnter}
+      />
+    </div>
+  );
+};
+
+export default ParagraphEditor;

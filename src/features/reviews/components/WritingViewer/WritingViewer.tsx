0d1e6bb223d965201d0cff0e6727f49451bf51fc
hyemin <hmpkk09@gmail.com>
2022-03-10 23:22:58 +0900

    feat: WritingViewer 컴포넌트 구현

--- src/features/reviews/components/WritingViewer/WritingViewer.tsx
+++ src/features/reviews/components/WritingViewer/WritingViewer.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import { useAppSelector } from '../../../../common/hooks/useRedux';
 import useParagraphsByWritingId from '../../hooks/useParagraphsByWritingId';
 import ParagraphEditor from '../ParagraphEditor/ParagraphEditor';
+import WritingViewerHeader from '../WritingViewerHeader.tsx/WritingViewerHeader';
+import styles from './WritingViewer.module.css';
 
 const WritingViewer = () => {
   const { writingId } = useParams();
@@ -10,12 +13,19 @@ const WritingViewer = () => {
     ({ writings }) => writings.byId[writingId as string].blocks[0]
   );
   const paragraphs = useParagraphsByWritingId(writingId as string);
+
   return (
-    <div>
-      {paragraphs.map((_, i) => (
-        <ParagraphEditor key={i} blockId={blockId} index={i} />
-      ))}
-    </div>
+    <>
+      <WritingViewerHeader />
+      <div className={styles.wrapper}>
+        <div className={styles.index} />
+        <div className={styles.paragraphs}>
+          {paragraphs.map((_, i) => (
+            <ParagraphEditor key={i} blockId={blockId} index={i} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 

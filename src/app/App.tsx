24e7c13bde398643586b43ffb55043586904ee23
hyemin <hmpkk09@gmail.com>
2022-03-12 11:29:02 +0900

    refactor: App 컴포넌트에서 페이지 컴포넌트 변수로 추출

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,48 +16,42 @@ import Loading from '../common/components/Loading/Loading';
 
 const App = () => {
   const isLoggedIn = !!useAppSelector(({ user }) => user.id);
+  const indexPage = isLoggedIn ? (
+    <Navigate replace to="/writing" />
+  ) : (
+    <Navigate replace to="/login" />
+  );
+
+  const writingPage = (
+    <AuthRequired>
+      <Writing />
+    </AuthRequired>
+  );
+
+  const reviewPage = (
+    <AuthRequired>
+      <Review />
+    </AuthRequired>
+  );
 
   return (
     <>
       <ErrorAlert />
       <Loading />
       <Routes>
-        <Route
-          path="/"
-          element={
-            isLoggedIn ? (
-              <Navigate replace to="/writing" />
-            ) : (
-              <Navigate replace to="/login" />
-            )
-          }
-        />
+        <Route path="/" element={indexPage} />
 
         <Route element={<Login />}>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/signup" element={<SignupForm />} />
         </Route>
 
-        <Route
-          path="/writing"
-          element={
-            <AuthRequired>
-              <Writing />
-            </AuthRequired>
-          }
-        >
+        <Route path="/writing" element={writingPage}>
           <Route index element={<WritingList responsive={false} />} />
           <Route path=":writingId" element={<BlockEditor />} />
         </Route>
 
-        <Route
-          path="/review"
-          element={
-            <AuthRequired>
-              <Review />
-            </AuthRequired>
-          }
-        >
+        <Route path="/review" element={reviewPage}>
           <Route index element={<WritingList responsive={false} />} />
           <Route path=":writingId" element={<WritingViewer />} />
         </Route>

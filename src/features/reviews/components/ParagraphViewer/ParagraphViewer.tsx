499be244db821aad8e8d315705df4eed03615b38
hyemin <hmpkk09@gmail.com>
2022-03-12 15:20:51 +0900

    feat: ParagraphViewer 컴포넌트 구현

---
+++ src/features/reviews/components/ParagraphViewer/ParagraphViewer.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ViewerLayout from '../../../../common/components/ViewerLayout/ViewerLayout';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../../../common/hooks/useRedux';
+
+import CommentableParagraph from '../../../comments/components/CommentableParagraph/CommentableParagraph';
+import { fetchWritingById } from '../../../writings/actions';
+import useParagraphsByWritingId from '../../hooks/useParagraphsByWritingId';
+import styles from './ParagraphViewer.module.css';
+
+const ParagraphViewer = () => {
+  const { writingId } = useParams<keyof WritingIdParam>() as WritingIdParam;
+  const writing = useAppSelector(({ writings }) => writings.byId[writingId]);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!writing) {
+      dispatch(fetchWritingById(writingId));
+    }
+  }, [writing]);
+
+  const paragraphs = useParagraphsByWritingId(writingId);
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>{writing.title}</h1>
+      <ViewerLayout>
+        {paragraphs?.map((paragraph, i) => (
+          <CommentableParagraph key={i} content={paragraph} index={i} />
+        ))}
+      </ViewerLayout>
+    </div>
+  );
+};
+
+export default ParagraphViewer;

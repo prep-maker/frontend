04ea64c6dd687c5a2b542d67d555ff87cf09c79f
hyemin <hmpkk09@gmail.com>
2022-03-13 17:40:09 +0900

    feat: FeedbackViewer에 writing prop 추가

--- src/features/reviews/components/FeedbackViewer/FeedbackViewer.tsx
+++ src/features/reviews/components/FeedbackViewer/FeedbackViewer.tsx
@@ -1,47 +1,36 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 
 import ViewerLayout from '../../../../common/components/ViewerLayout/ViewerLayout';
-import {
-  useAppDispatch,
-  useAppSelector,
-} from '../../../../common/hooks/useRedux';
 import CommentableParagraph from '../../../comments/components/CommentableParagraph/CommentableParagraph';
-import { fetchWritingById } from '../../../writings/actions';
+import { Writing } from '../../../writings/writingsSlice';
 import useParagraphsByWritingId from '../../hooks/useParagraphsByWritingId';
 import FeedbackViewerHeader from '../FeedbackViewerHeader/FeedbackViewerHeader';
 import styles from './FeedbackViewer.module.css';
 
-const FeedbackViewer = () => {
-  const { writingId } = useParams<keyof WritingIdParam>() as WritingIdParam;
-  const writing = useAppSelector(({ writings }) => writings.byId[writingId]);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (!writing) {
-      dispatch(fetchWritingById(writingId));
-    }
-  }, [writing]);
-
-  const paragraphs = useParagraphsByWritingId(writingId);
-  const blockId = writing.blocks[0];
-  const comments = paragraphs.flatMap((paragraph) => paragraph.comments);
+const FeedbackViewer = ({ writing }: { writing: Writing }) => {
+  const paragraphs = useParagraphsByWritingId(writing.id);
+  const blockId = writing?.blocks[0];
+  const comments = paragraphs?.flatMap((paragraph) => paragraph.comments);
 
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>{writing.title}</h1>
-      <FeedbackViewerHeader comments={comments} />
-      <ViewerLayout>
-        {paragraphs?.map((paragraph, i) => (
-          <CommentableParagraph
-            key={i}
-            paragraph={paragraph}
-            index={i}
-            blockId={blockId}
-          />
-        ))}
-      </ViewerLayout>
-    </div>
+    <>
+      (
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>{writing.title}</h1>
+        <FeedbackViewerHeader comments={comments} />
+        <ViewerLayout>
+          {paragraphs?.map((paragraph, i) => (
+            <CommentableParagraph
+              key={i}
+              paragraph={paragraph}
+              index={i}
+              blockId={blockId}
+            />
+          ))}
+        </ViewerLayout>
+      </div>
+      )
+    </>
   );
 };
 
--- src/pages/Feedback/Feedback.tsx
+++ src/pages/Feedback/Feedback.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import Header from '../../common/components/Header/Header';
+import Loading from '../../common/components/Loading/Loading';
+import { useAppSelector } from '../../common/hooks/useRedux';
 import FeedbackViewer from '../../features/reviews/components/FeedbackViewer/FeedbackViewer';
+import { fetchWritingById } from '../../features/writings/actions';
 
 const Feedback = () => {
+  const { writingId } = useParams<keyof WritingIdParam>() as WritingIdParam;
+  const writing = useAppSelector(({ writings }) => writings.byId[writingId]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!writing) {
+      dispatch(fetchWritingById(writingId));
+    }
+  }, [writing]);
+
   return (
-    <div>
-      <Header isLoggedIn={false} />
-      <FeedbackViewer />
-    </div>
+    <>
+      {writing ? (
+        <div>
+          <Header isLoggedIn={false} />
+          <FeedbackViewer writing={writing} />
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
